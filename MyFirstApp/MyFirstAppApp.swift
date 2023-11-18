a30fb60fe1cf7192ac7292aa07c17f19b7a7c0b6
Hamza Masood <masoodhamza2021@gmail.com>
2023-11-18 11:55:16 +0100

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Choudhary Hamza on 18.11.23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
